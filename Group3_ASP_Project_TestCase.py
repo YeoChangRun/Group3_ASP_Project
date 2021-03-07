c4764d4c06737f87f79f6fa467fd523a5130316a
YeoChangRun <75964668+YeoChangRun@users.noreply.github.com>
2021-03-08 01:14:29 +0800

    Add files via upload

---
+++ Group3_ASP_Project_TestCase.py
@@ -0,0 +1,9 @@
+import unittest
+import Group3_ASP_Project as prog
+
+class Group3_ASP_Project_TestCase(unittest.TestCase):
+    def test_sum(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
